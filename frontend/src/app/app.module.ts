a8febddcca6630b4555fc5b8da7c5254170b86cf
Cristian Beltran <cbeltranc6@gmail.com>
2020-10-20 22:53:40 -0300

    Estructura de componentes

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrincipalComponent } from './components/principal/principal.component';
+import { ConsolaComponent } from './components/consola/consola.component';
+import { GraficasComponent } from './components/graficas/graficas.component';
+import { HistorialComponent } from './components/historial/historial.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrincipalComponent,
+    ConsolaComponent,
+    GraficasComponent,
+    HistorialComponent
   ],
   imports: [
     BrowserModule,
