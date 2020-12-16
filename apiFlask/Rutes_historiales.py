5eab7ee65ceb7db37845d59a7ef5047f3781c46f
Cristian Beltran <cbeltranc6@gmail.com>
2020-12-16 01:19:45 -0300

    Actualiza rutas

--- apiFlask/Rutes_historiales.py
+++ apiFlask/Rutes_historiales.py
@@ -2,42 +2,13 @@ from ConexionDB import app, historiales
 from bson import ObjectId
 import json
 from bson.json_util import dumps
-from const import histos, ruts
+from const import histos, ruts, sesiones_global
 
 
-@app.route('/ciudades', methods=['GET'])
-def findAllCiudades(): 
-    query = historiales.find()
-    salida = []
-    for x in query:
-        salida.append(x)    
-    ciudades = []
-    for i in salida:
-        if i['ciudad'] not in ciudades:
-            ciudades.append(i['ciudad'])
-    return JSONEncoder().encode(ciudades)
 
-# devuelve un array con todos los nombres de las distintas sesiones
-@app.route('/sesiones_medica', methods=['GET']) 
-def findAllSesionesClinicas(): 
-    query = historiales.find()
-    sesiones = []
-    for i in query:
-        for s in i['sesiones_medica']:
-            if s['nombre_sesion'] not in sesiones:
-                sesiones.append(s['nombre_sesion'])
-    return JSONEncoder().encode(sesiones)
 
-@app.route('/sesiones_medica/arquetipos', methods=['GET']) 
-def findAllArquetiposSesionesClinicas(): 
-    query = historiales.find()      
-    arquetipos = []
-    for i in query:
-        for s in i['sesiones_medica']:            
-                for a in s['arquetipos']:
-                    for arquetipo in a:
-                        if arquetipo not in arquetipos:
-                            arquetipos.append(arquetipo)
+@app.route('/arquetipos', methods=['GET']) 
+def findAllArquetiposSesionesClinicas():     
     return dumps(arquetipos)
 
 @app.route('/historiales', methods=['GET']) 
@@ -48,11 +19,3 @@ def findAllHistoriales():
 def findAllRuts(): 
     return dumps(ruts)
 
-
-@app.route('/historiales/ciudad/<ciudad>', methods=['GET']) 
-def findAllHistorialesCiudad(ciudad): 
-    query = historiales.find({'ciudad': str(ciudad)})
-    salida = []
-    for x in query:
-        salida.append(x)
-    return JSONEncoder().encode(salida)
--- apiFlask/const.py
+++ apiFlask/const.py
@@ -3,13 +3,15 @@ historiales_global = historiales.find()
 ruts= []
 histos = []
 sesiones_global = []
+arquetipos = []
 for histo in historiales_global:
     ruts.append(histo['rut'])
     histos.append(histo)
+    i = 0
+    for s in histo['sesiones_medica']:
+        sesion = {'id':i,'nombre_sesion': s['nombre_sesion'],'profesionales_que_atendieron': histo['profesionales_que_atendieron'][i],  'profesion': s['profesion'], 'fecha': s['fecha'], 'centro_salud':s['centro_salud'], 'nombre_profesional': s['nombre_profesional']}
+        sesiones_global.append(sesion)
+        i += 1
 
 
-
-
-
-
-print("Termino descargar datos")
+print("Termino descargar datos!!!!!!!!!!!!!!!!!!!!!")
